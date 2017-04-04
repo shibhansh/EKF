a144580e956a1484adeabc01052f8f5ecf9ecc9e
Shibhansh Dohare <shibhanshdohare1997@gmail.com>
2017-04-05 01:12:14 +0530

    fix/update.cpp

--- update.cpp
+++ update.cpp
@@ -1,45 +1,48 @@
-function [x,P]= EKF_update(x,P,z,R,idf,lm)
- 
-% Inputs:
-%   x, P -  state and covariance
-%   z, R - range-bearing measurements and covariances
-%   idf - feature index for each z
- 
-% Outputs:
-%   x, P - updated state and covariance
+// #include <predict>
+#include <iostream>
+#include <vector>
+#include <Eigen/Dense>
 
+using namespace Eigen;
+using namespace std;
 
-% <---------------------------- TO DO -------------------------->
-size_idf = size(idf,1);
-if numel(idf) == 0
-	return;
-else
-    i = 0;
-	for i=1:size_idf
-		l = lm(:,idf(i));
-		z(2,i) = pi_to_pi(z(2,i));
-		dist = sqrt((x(1)-l(1))^2+(x(2)-l(2))^2);
-		z_predicted = [dist;	pi_to_pi(atan2(l(2)-x(2),l(1)-x(1)) - x(3))];
+template <typename Derived>
 
-		H = [(x(1)-l(1))/dist, (x(2)-l(2))/dist, 0;
-			 -(x(2)-l(2))/dist^2, (x(1)-l(1))/dist^2, -1];
+Matrix3f void EKF_update(float* x, const Ref<const Matrix3f>& P,float *z,const Ref<const Matrix3f>& R,const Ref<const VectorXf>& idf,int lm){
+	//  Inputs:
+	//    x, P -  state and covariance
+	//    z, R - range-bearing measurements and covariances
+	//    idf - feature index for each z
+	//  Outputs:
+	//    x, P - updated state and covariance
+	// % <---------------------------- TO DO -------------------------->
 
-		S = H * P * H' + R;
-		K = P*H'*pinv(S);
-		a = (z(:,i) - z_predicted);
-		a(2) = pi_to_pi(a(2));
-		x = x + K*a;
-		x(3) = pi_to_pi(x(3));
-		P = (eye(3) - K * H)*P;
-	 
-	end        
- 
-end        
+	int size_idf = 3;
+	size_idf = size(idf,1);
+	if(numel(idf) == 0)
+		return;
+	else{
+	    int i = 0;
+		for( i=1, i<=size_idf, i++){
+			l = lm(:,idf(i));
+			z(2,i) = pi_to_pi(z(2,i));
+			float dist = sqrt((x[1]-l[1])^2+(x[2]-l[2])^2);
+			VectorXf z_predicted (dist,	pi_to_pi(atan2(l(2)-x(2),l(1)-x(1)) - x(3)));
 
+			Matrix<float, 3,2>	H;
+			H << (x(1)-l(1))/dist, (x(2)-l(2))/dist, 0,
+				-(x(2)-l(2))/dist^2, (x(1)-l(1))/dist^2, -1;
 
-     
-    
-    
-  
-
-         
+			Matrix<float, 3,2>	H;
+			S = H * P * H.transpose() + R;
+			Matrix<float, 3,2>	K;
+			K = P*H.transpose()*pseudoInverse(S);
+			a = (z(:,i) - z_predicted);
+			a(2) = pi_to_pi(a(2));
+			x = x + K*a;
+			x(3) = pi_to_pi(x(3));
+			P = (MatrixXd::Identity(3,3) - K * H)*P;
+		}
+	}
+	return K;
+}
